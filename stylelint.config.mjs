e2e02cd30b92f9a716a06b29130b5ff6fd68f861
Sabyrzhan Zhaslanuly <zhaslanulydev@gmail.com>
2026-08-13 03:27:18 +0500

    chore: add CSS normalization

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import("stylelint").Config} */
 export default {
   extends: ['stylelint-config-standard'],
+  rules: {
+    'custom-property-pattern': null,
+    'selector-not-notation': null,
+    'comment-empty-line-before': null,
+    'value-keyword-case': null,
+  },
 }
