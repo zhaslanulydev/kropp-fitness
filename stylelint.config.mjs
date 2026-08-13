cdf57c479746c93ce8497ebc874b9bbe66bf7b26
Sabyrzhan Zhaslanuly <zhaslanulydev@gmail.com>
2026-08-13 15:50:25 +0500

    chore: prepare foundation (#2)
    * docs: add README
    
    * chore: add CSS normalization
    
    * chore: add custom fonts
    
    * chore: add CSS variables and update Stylelint config
    
    * chore: add global CSS styles
    
    * chore: add utility CSS classes
    
    * chore: add typography utilities and update Stylelint config
    
    * chore: add backdrop title utility and icon asset

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import("stylelint").Config} */
 export default {
   extends: ['stylelint-config-standard'],
+  rules: {
+    'custom-property-pattern': null,
+    'selector-not-notation': null,
+    'comment-empty-line-before': null,
+    'value-keyword-case': null,
+    'font-family-name-quotes': null,
+    'custom-property-empty-line-before': null,
+    'at-rule-empty-line-before': null,
+    'media-feature-range-notation': null,
+    'import-notation': 'string',
+  },
 }
